28bf5ffd2f34d415b0ad649237816be3a2d6aba3
Adam Lopez <adamf04@gmail.com>
2020-05-05 14:50:53 -0400

    created a to do list using HTML, CSS, and vanilla Javascript.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+    <title>Adam's To do list</title>
+</head>
+<body>
+<div id="myDIV" class="header">
+<h2>To Do List</h2>
+
+<input type="text" id="myInput" placeholder="Title..">
+
+<span onclick="newElement()" class="addBtn">Add</span>
+</div>
+
+<ul id="myUL"> </ul>
+    
+</body>
+</html>
